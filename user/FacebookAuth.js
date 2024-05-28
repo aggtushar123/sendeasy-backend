29f63f50e25494c06764b8d411a4b4831556facf
Tushar Aggaqrwal <aggtushar123@gmail.com>
2024-05-28 23:06:40 +0530

    Google Auth Fixed

--- user/FacebookAuth.js
+++ user/FacebookAuth.js
@@ -11,21 +11,48 @@ router.get(
 router.get(
   '/facebook/callback',
   passport.authenticate('facebook', {
-    successRedirect: 'http://localhost:3000/loggedInSuccess',
+    successRedirect: 'http://localhost:3000/auth/google/success',
     failureRedirect: '/login/failed',
   })
 );
 
-router.get('/login/success', (req, res) => {
-  // console.log(req);
+router.get("/login/success", async (req, res) => {
   if (req.user) {
-    res.status(200).json({
-      error: false,
-      message: 'Successfully logged in',
-      user: req.user,
-    });
+    try {
+      const { email } = req.user;
+      const userExists = await User.findOne({ email });
+
+      if (userExists) {
+        res.status(200).json({
+          user: userExists,
+        });
+      } else {
+        
+        const user = await new User({
+          email: req.user.email,
+          fName: req.user.displayName,
+          verified: true,
+          isAdmin: false,
+          profilePicture: req.user.picture,
+        });
+        const savedUser = await user.save();
+
+        // Generate a token for the new user
+        const token = generateToken(savedUser._id);
+
+        // Update the user with the generated token
+        savedUser.token = token;
+        const updatedUser = await savedUser.save();
+
+        // Send the updated user to the frontend
+        res.status(200).json(updatedUser);
+      }
+    } catch (error) {
+      console.log("Error in login/success route:", error);
+      res.status(500).json({ error: true, message: "Server error" });
+    }
   } else {
-    res.status(403).json({ error: true, message: 'Not authorized' });
+    res.status(403).json({ error: true, message: "Not authorized" });
   }
 });
 
